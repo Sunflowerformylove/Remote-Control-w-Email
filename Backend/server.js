7ef6c6e5f65dbc838d1f511c9ee5162fff1e9329
Hai Do <dodinhhai29904@gmail.com>
2023-10-19 00:10:39 +0700

    install react

--- Backend/server.js
+++ Backend/server.js
@@ -6,6 +6,35 @@ const multer = require("multer");
 const app = express();
 const PORT = 2909;
 
-app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
+const mailOption = {
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: 'your email',
+        pass: 'your password'
+    },
+    tls: {
+        rejectUnauthorized: false,
+        minVersion: "TLSv1.2",
+    },
+}
+
+//middleware
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true
+}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(express.static('Public'));
+
+app.listen(PORT, (err) => 
+{ 
+    if (err) console.error(err.message);
+    console.log(`Server is running on PORT ${PORT}`)
+});
 
 // Path: Backend/server.js
