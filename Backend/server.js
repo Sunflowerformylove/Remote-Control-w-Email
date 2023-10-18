22e7e09414fa58264740ce4b76893f1e41f6a2aa
Hai Do <dodinhhai29904@gmail.com>
2023-10-18 22:58:43 +0700

    follow-up files for the prev commit

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
+const multer = require("multer");
 const app = express();
 const PORT = 2909;
 
