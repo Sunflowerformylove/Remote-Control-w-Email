b8c1c74e2866aed9cd692162ee8102c3dae75065
Sang-Nguyen-Phuoc <nguyenphuocsang04012004@gmail.com>
2023-10-23 20:48:28 +0700

    built Welcome page, loading page included before rendering main page

--- Frontend/src/Components/Main.js
+++ Frontend/src/Components/Main.js
@@ -1,12 +1,32 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Menu from "./Menu";
 import "../Styles/Main.css";
+import Loading from './Loading';
 
-export default function Main(){
-    return(
-        <div className="mainContainer">
-            <Menu></Menu>
-        </div>
+export default function Main() {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        // Simulate a 1-second delay
+        const timer = setTimeout(() => {
+            setLoading(false);
+        }, 1000);
+
+        // Clear the timeout if the component is unmounted
+        return () => clearTimeout(timer);
+    }, []);
+
+    return (
+        <>
+            {loading ? (
+                <Loading />
+            ) : (
+                <div className="mainContainer">
+                    <Menu></Menu>
+                </div>
+
+            )}
+        </>
     );
 }
--- Frontend/src/Components/Welcome.js
+++ Frontend/src/Components/Welcome.js
@@ -1,7 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import '../Styles/Welcome.css';
+import Loading from './Loading';
 
+export default function Welcome(props) {
+    const [loading, setLoading] = useState(true);
 
-export default function Welcome(props){
-    return (<>
+    useEffect(() => {
+        // Simulate a 1-second delay
+        const timer = setTimeout(() => {
+            setLoading(false);
+        }, 1000);
 
-    </>)
+        // Clear the timeout if the component is unmounted
+        return () => clearTimeout(timer);
+    }, []);
+
+    return (
+        <>
+            {loading ? (
+                <Loading />
+            ) : (
+                <div className='container'>
+                    <div className='container-welcome showcase'>
+                        <img className='background-img' src='https://picsum.photos/200' alt='background' />
+                        <h1>Welcome to our webiste</h1>
+                        <div className='introduction'>Control personal user remotely through email.</div>
+                    </div>
+                    <div className='container-welcome button-block'>
+                        <Link to='/main'>
+                            <button className='button-52' role='button'>
+                                Enter
+                            </button>
+                        </Link>
+                    </div>
+                </div>
+            )}
+        </>
+    );
 }
