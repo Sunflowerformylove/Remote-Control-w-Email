f66792489685f8d6541e4042d2936948dd434f3d
Hai Do <dodinhhai29904@gmail.com>
2023-10-27 21:26:35 +0700

    add function and style for mail, add mail format when choose a functionality

--- Frontend/src/Components/Mail.js
+++ Frontend/src/Components/Mail.js
@@ -1,7 +1,9 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import "../Styles/Mail.css";
 import { IonIcon } from "@ionic/react";
 import * as Icon from "ionicons/icons";
+import { toastError, toastSuccess } from "./Toast";
+const lodash = require("lodash");
 
 export default function Mail(props) {
     const [mail, setMail] = useState("");
@@ -9,6 +11,19 @@ export default function Mail(props) {
     const [subject, setSubject] = useState("");
     const [command, setCommand] = useState("");
     const [description, setDescription] = useState("");
+    const [password, setPassword] = useState("");
+    const [form, setForm] = useState(null);
+    const [PID, setPID] = useState(0);
+    const [folderName, setFolderName] = useState("");
+    const [task_number, setTask_number] = useState(0);
+    const [second, setSecond] = useState(0);
+    const [termCommand, setTermCommand] = useState("");
+    const [eye, setEye] = useState(Icon.eyeOff);
+    const [cmdArg, setCmdArg] = useState("");
+    const passwordRef = useRef(null);
+    const eyeRef = useRef(null);
+    const mailRef = useRef(null);
+    const cmdArgRef = useRef(null);
 
     function updateTime() {
         setInterval(() => {
@@ -16,86 +31,140 @@ export default function Mail(props) {
         }, 1000);
     }
 
-    const commands = {
-        shutdown: "shutdown",
-        sleep: "sleep",
-        MAC_IP: "MAC/IP",
-        task: "task",
-        terminal: "terminal",
-        screenshot: "screenshot",
-        keylogger: "keylogger",
-        terminate: "terminate",
-        folder: "folder tree",
-        system: "system",
-    }
-
-    function setMailContent(index, command, time = 0, task_number = 0, PID = 0) {
+    function setMailContent(index, command, time = 0, task_number = 0, PID = 0, folderName = "C:/Users/dodin/Desktop") {
         switch (index) {
             case 0:
-                setMail(commands.shutdown);
                 setSubject("[RDCVE] Shutdown");
                 setCommand("Shutdown " + time);
-                setDescription("Shutdown the computer immediately");
+                setDescription("Shutdown the computer after the given time in seconds");
                 break;
             case 1:
-                setMail(commands.sleep);
                 setSubject("[RDCVE] Sleep");
-                setCommand("Sleep" + time);
-                setDescription("Put the computer to sleep");
+                setCommand("Sleep " + time);
+                setDescription("Put the computer to sleep after the given time in seconds");
                 break;
             case 2:
-                setMail(commands.MAC_IP);
                 setSubject("[RDCVE] MAC/IP");
                 setCommand("MAC/IP");
                 setDescription("Get the MAC and IP address of the computer");
                 break;
             case 3:
-                setMail(commands.task);
                 setSubject("[RDCVE] Task Manager");
-                setCommand("Task Manager " + task_number);
+                setCommand("Task Manager " + task_number); // 0 for all, 1 for running, 2 for not responding, 3 for RAM, 4 for CPU
                 setDescription("Get the list of running processes with ports and PIDs");
                 break;
             case 4:
-                setMail(commands.terminal);
                 setSubject("[RDCVE] Terminal");
-                setCommand("Terminal " + command);
+                setCommand("Terminal ", command);
                 setDescription("Run a command in the terminal");
                 break;
             case 5:
-                setMail(commands.screenshot);
+                ;
                 setSubject("[RDCVE] Screenshot");
                 setCommand("Screenshot");
                 setDescription("Take a screenshot of the computer");
                 break;
             case 6:
-                setMail(commands.keylogger);
                 setSubject("[RDCVE] Keylogger");
                 setCommand("Keylogger");
                 setDescription("Get the keystrokes of the computer");
                 break;
             case 7:
-                setMail(commands.terminate);
                 setSubject("[RDCVE] Terminate");
                 setCommand("Terminate " + PID);
                 setDescription("Terminate the a task with the given PID");
                 break;
             case 8:
-                setMail(commands.folder);
                 setSubject("[RDCVE] Folder Tree");
                 setCommand("Folder Tree" + folderName);
-                setDescription("Get the folder tree of the computer");
+                setDescription("Get the folder tree of the computer. The computer username is: dodin");
                 break;
             case 9:
-                setMail(commands.system);
                 setSubject("[RDCVE] System Info");
                 setCommand("System Info");
-                setDescription("Get the system info of the computer");
+                setDescription("Get the system info of the computer.");
                 break;
+            default:
+                toastError("Error: Invalid command")
+                break;
+        }
     }
+
+    function passwordVisibility() {
+        passwordRef.current.type = passwordRef.current.type === "password" ? "text" : "password";
+        setEye(eye === Icon.eyeOff ? Icon.eye : Icon.eyeOff);
+    }
+
+    function setFormContent() {
+        setMail(mailRef.current.value);
+        setForm({
+            sender: mail,
+            password: passwordRef.current.value,
+        });
+    }
+
+    function setCmdArgContent() {
+        setCmdArg(cmdArgRef.current.value);
+    }
+
+    function checkCmdArg(index) {
+        switch (index) {
+            case 1:
+                setSecond(cmdArg);
+                break;
+            case 2:
+                setSecond(cmdArg);
+                break;
+            case 3:
+                setTask_number(cmdArg);
+                break;
+            case 4:
+                setTermCommand(cmdArg);
+                break;
+            case 5:
+                if (cmdArg.length !== 0) {
+                    toastError("Error: Command argument is not needed");
+                    return false;
+                }
+                break;
+            case 6:
+                setPID(cmdArg);
+                break;
+            case 7:
+                if (folderName.length === 0) {
+                    toastError("Error: Invalid folder name");
+                    return false;
+                }
+                setFolderName(cmdArg);
+                break;
+            case 8:
+                if (cmdArg.length !== 0) {
+                    toastError("Error: Command argument is not needed");
+                    return false;
+                }
+                break;
+            case 9:
+                if (cmdArg.length !== 0) {
+                    toastError("Error: Command argument is not needed");
+                    return false;
+                }
+                return true;
+            default:
+                break;
+        }
+    }
+
+    useEffect(() => {
+        setMailContent(props.chosenFunctionality, command, second, task_number, PID, folderName);
+    }, [props.chosenFunctionality, command, second, task_number, PID, folderName]);
 
     useEffect(() => {
         updateTime();
     }, []);
+
+    useEffect(() =>{
+        checkCmdArg(props.chosenFunctionality);
+    }, [cmdArg, props.chosenFunctionality])
 
     return (<>
         <div className="mailContainer">
@@ -106,17 +175,27 @@ export default function Mail(props) {
                     <div className="copyButton">Copy</div>
                 </div>
                 <div className="mailSender">
-                    <div className="mailSenderText">From: </div>
-                    <input type={"text"} className="senderEmail"></input>
+                    <div onInput={setFormContent} ref={mailRef} className="mailSenderText">From: </div>
+                    <input ref={passwordRef} type={"text"} className="senderEmail"></input>
+                </div>
+                <div className="senderPassword">
+                    <div className="senderPasswordText">Password: </div>
+                    <input onInput={setFormContent} ref={passwordRef} type={"password"} className="senderPasswordInput"></input>
+                    <IonIcon ref={eyeRef} onClick={passwordVisibility} icon={eye} className={"passwordState"}></IonIcon>
                 </div>
                 <div className="subjectBox">
                     <div className="subjectText">Subject: </div>
-                    <div className="dynamicSubject">[RDCVE] Shutdown</div>
+                    <div className="dynamicSubject">{subject}</div>
                     <div className="copyButton">Copy</div>
                 </div>
                 <div className="mailContentContainer">
                     <div className="mailContent">
-                        <div className="command">Command: </div>
+                        <div className="command">Command: {command} </div>
+                        <br></br>
+                        <div className="commandArgument">
+                            <div className="commandArText">Command argument:</div>
+                            <input onInput={setCmdArgContent} ref={cmdArgRef} type="text" className="commandArInput" />
+                        </div>
                         <br></br>
                         <div className="date">Time sent: {time}</div>
                         <br></br>
--- Frontend/src/Components/Toast.js
+++ Frontend/src/Components/Toast.js
@@ -2,20 +2,25 @@ import { toast } from "react-toastify";
 export function toastError(message) {
     toast.error(message, {
         position: "top-center",
-        autoClose: 5000,
+        autoClose: 3000,
         hideProgressBar: false,
         closeOnClick: true,
         pauseOnHover: true,
         draggable: true,
         progress: undefined,
         theme: "dark",
+        style: { 
+            fontSize: "1.8rem",
+            fontFamily: "Poppins, sans-serif",
+            fontWeight: 600,
+        },
     });
 }
 
 export function toastSuccess(message, callback) {
     toast.success(message, {
         position: "top-center",
-        autoClose: 5000,
+        autoClose: 3000,
         hideProgressBar: false,
         closeOnClick: true,
         pauseOnHover: true,
@@ -23,6 +28,11 @@ export function toastSuccess(message, callback) {
         progress: undefined,
         theme: "dark",
         onClose: callback,
+        style: { 
+            fontSize: "1.8rem",
+            fontFamily: "Poppins, sans-serif",
+            fontWeight: 600,
+        },
     });
 }
 
@@ -31,5 +41,16 @@ export function toastPromise(Promise, message1, message2, message3) {
         pending: message1,
         success: message2,
         error: message3,
+        theme: "dark",
+        style: { 
+            fontSize: "1.8rem",
+            fontFamily: "Poppins, sans-serif",
+            fontWeight: 600,
+        },
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
     });
 }
