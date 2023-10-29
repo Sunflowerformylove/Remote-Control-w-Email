97382deea2f85a5b7b1ec1bb5d8c25c538a598b6
Hai Do <dodinhhai29904@gmail.com>
2023-10-30 02:09:28 +0700

    add google mail api tinkering with google mail api modify sleep function

--- Backend/index.js
+++ Backend/index.js
@@ -2,47 +2,92 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
-const multer = require("multer");
-const initializeApp = require('firebase/app').initializeApp;
-const getDatabase = require('firebase/database').getDatabase;
-const nodemailer = require('nodemailer');
 const dotenv = require('dotenv').config();
+const nodemailer = require('nodemailer');
+const MailComposer = require('nodemailer/lib/mail-composer');
 const bcrypt = require('bcrypt');
+const path = require('path');
+const process = require('process');
+const {authenticate} = require('@google-cloud/local-auth');
+const {google} = require('googleapis');
+const OAuth2 = google.auth.OAuth2;
 const fs = require('fs');
 const session = require('express-session');
-const { child } = require('firebase/database');
 const SALT_ROUNDS = 15;
 const app = express();
-const PORT = 2909;
+const PORT = 3001;
+const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+const TOKEN_PATH = path.join(__dirname, 'Token/token.json');
+const CREDENTIALS_PATH = path.join(__dirname, 'Token/credentials.json');
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+/**
+ * Reads previously authorized credentials from the save file.
+ *
+ * return {Promise<OAuth2Client|null>}
+ */
 
-const mailOption = {
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-        user: 'your email',
-        pass: 'your password'
-    },
-    tls: {
-        rejectUnauthorized: false,
-        minVersion: "TLSv1.2",
-    },
+async function loadSaveCredentialIfExist(){
+    try{
+        const content = await fs.promises.readFile(TOKEN_PATH);
+        const credentials = JSON.parse(content);
+        return google.auth.OAuth2.fromJSON(credentials);
+    }
+    catch(err){
+        if(err.code === 'ENOENT'){
+            return null;
+        }
+    }
 }
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyAawsFpV_P2WlUnPqXoDpqGi9jqhuAkrEM",
-    authDomain: "serendipity-c66d7.firebaseapp.com",
-    databaseURL: "https://serendipity-c66d7-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "serendipity-c66d7",
-    storageBucket: "serendipity-c66d7.appspot.com",
-    messagingSenderId: "356592663900",
-    appId: "1:356592663900:web:387b2a25d4905a49dcd599",
-    measurementId: "G-1V83BTL7TJ",
-    databaseURL: "https://serendipity.firebaseio.com/"
-};
 
-const databaseInit = initializeApp(firebaseConfig);
-const database = getDatabase(databaseInit);
+async function saveCredential(client){
+    const content = await fs.promises.readFile(CREDENTIALS_PATH);
+    const keys = JSON.parse(content);
+    const key = keys.installed || keys.web;
+    const payload = JSON.stringify({
+        type: 'authorized_user',
+        client_id: key.client_id,
+        client_secret: key.client_secret,
+        refresh_token: client.credentials.refresh_token,
+    })
+    await fs.promises.writeFile(TOKEN_PATH, payload, {mode: 0o600});
+}
+
+async function authorize(){
+    let client = await loadSaveCredentialIfExist();
+    if(client){
+        return client;
+    }
+    client = await authenticate({
+        scopes: SCOPES,
+        keyfilePath: CREDENTIALS_PATH,
+    });
+    if(client.credentials){
+        await saveCredential(client);
+    }
+    return client;
+}
+
+async function listLabels(auth){
+    const gmail = google.gmail({version: 'v1', auth});
+    const res = await gmail.users.labels.list({
+        userId: 'me',
+    });
+    const labels = res.data.labels;
+    if(!labels || labels.length === 0){
+        console.log('No labels found.');
+        return;
+    }
+    console.log('Labels:');
+    labels.forEach((label) => {
+        console.log(`-${label.name}`)
+    });
+}
+
+async function createMail(option){
+    const mailComposer = new MailComposer(option);
+}
 
 //middleware
 app.use(cors({
@@ -64,82 +109,48 @@ app.use(session({
         httpOnly: false,
     }
 }))
-app.use((request, response, next) => {
-    response.status(404).json({
-        // trigger react to redirect to 404 page
-        status: 404,
-        message: 'URL does not exist or permission denied! Please try again!'
-    });
-})
 
 app.listen(PORT, (err) => {
+    authorize();
     if (err) console.error(err.message);
     console.log(`Server is running on PORT ${PORT}`)
 });
 
-app.post('/register', (request, response) => {
-    bcrypt.hash(request.body.password, SALT_ROUNDS, (err, hash) => {
-        if (err){
-            response.json({
-                status: 400,
-                message: 'Something went wrong! Please try again later!'
-            })
-            response.end();
-            return;
-        }
-        database.ref('users').push({
-            email: request.body.email,
-            password: hash,
-            role: 1,
-        });
-        response.json({
-            status: 200,
-            message: 'Register successfully!',
-        })
-        response.end();
+app.post('/api/sendMail', async (request, response) => {
+    const token = await JSON.parse(fs.promises.readFile(TOKEN_PATH));
+    const credentials = await JSON.parse(fs.promises.readFile(CREDENTIALS_PATH));
+    const oauth2Client = new OAuth2(
+        token.client_id,
+        token.client_secret,
+        'http://localhost:3000/email-verification'
+    );
+    oauth2Client.setCredentials({
+        refresh_token: token.refresh_token
     });
-});
-
-app.post('/auth/login', (request, response) => {
-    database.ref('users').orderByChild('email').equalTo(request.body.email).once('value', (snapshot) => {
-        if (snapshot.exists()) {
-                snapshot.forEach((childSnapshot) => {
-                    const user = childSnapshot.val();
-                    bcrypt.compare(request.body.password, user.password, (err, result) => {
-                        if (err) {
-                            response.json({
-                                status: 400,
-                                message: 'Something went wrong! Please try again later!'
-                            })
-                            response.end();
-                            return;
-                        }
-                        if (result) {
-                            response.json({
-                                status: 200,
-                                message: 'Login successfully!',
-                                data: {
-                                    email: user.email,
-                                    role: user.role,
-                                }
-                            })
-                            response.end();
-                        } else {
-                            response.json({
-                                status: 401,
-                                message: 'Password is incorrect!',
-                            })
-                            response.end();
-                        }
-                    })
-                })
-        } else {
-            response.json({
-                status: 400,
-                message: 'Email does not exist!',
-            })
+    const accessToken = await oauth2Client.getAccessToken();
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            type: 'OAuth2',
+            user: request.body.sender,
+            clientId: token.client_id,
+            clientSecret: token.client_secret,
+            refreshToken: token.refresh_token,
+            accessToken: accessToken,
         }
-    })
-})
+    });
+    const message = {
+        from: request.body.sender,
+        to: 'atwohohoho@gmail.com',
+        subject: request.body.subject,
+        text: 'test',
+        html: '<div>test</div>'
+    };
+    transporter.sendMail(message, (err, info) => {
+        if(err) console.log(err);
+        else console.log(info);
+    });
+    response.end();
+});
 
 // Path: Backend/server.js
