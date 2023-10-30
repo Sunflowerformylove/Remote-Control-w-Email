6876f08d7d02cb91791f78d266d6910faa703ab1
Anh Thư <blathu22@clc.fitus.edu.vn>
2023-10-30 11:58:49 +0700

    Update Server.py and Rewrite ScreenShots.py

--- Server/Source/ScreenShots.py
+++ Server/Source/ScreenShots.py
@@ -1,21 +1,13 @@
 # Python program to take 
 # screenshots 
   
-  
 import numpy as np 
 import cv2 
 import pyautogui 
-   
+import time
   
-# take screenshot using pyautogui 
-image = pyautogui.screenshot() 
-   
-# since the pyautogui takes as a  
-# PIL(pillow) and in RGB we need to  
-# convert it to numpy array and BGR  
-# so we can write it to the disk 
-image = cv2.cvtColor(np.array(image), 
-                     cv2.COLOR_RGB2BGR) 
-   
-# writing it to the disk using opencv 
-cv2.imwrite("image1.png", image) 
+def take_screenshots(sec):
+    time.sleep(sec)  # Wait for the specified seconds
+    image = pyautogui.screenshot()  # Take the screenshot
+    image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)  # Convert to BGR
+    cv2.imwrite("image1.png", image)  # Save the screenshot as "image1.png"
