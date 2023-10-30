f06c8e6816fde37251800bd3271cc827156a00f8
ankthuw <127480096+ankthuw@users.noreply.github.com>
2023-10-30 13:56:01 +0700

    update Server login

--- Server/Source/Server.py
+++ Server/Source/Server.py
@@ -15,16 +15,20 @@ from googleapiclient.errors import HttpError
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://mail.google.com/']
 
-def main():
+creds = None
+service = None
+
+def buildService():
     """Shows basic usage of the Gmail API.
     Lists the user's Gmail labels.
     """
-    creds = None
+    global creds
+    global service
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    if os.path.exists('../../token.json'):
+        creds = Credentials.from_authorized_user_file('../../token.json', SCOPES)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
@@ -34,9 +38,11 @@ def main():
                 'Server\\Assets\\JSON\\credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('token.json', 'w') as token:
+        with open('../../token.json', 'w') as token:
             token.write(creds.to_json())
+    service = build('gmail', 'v1', credentials=creds)
 
+def main():
     # try:
     #     # Call the Gmail API
     #     service = build('gmail', 'v1', credentials=creds)
@@ -53,10 +59,12 @@ def main():
     # except HttpError as error:
     #     # TODO(developer) - Handle errors from gmail API.
     #     print(f'An error occurred: {error}')
+    global creds
+    global service
+    buildService()
     while True:
         try:
             # Call the Gmail API
-            service = build('gmail', 'v1', credentials=creds)
             results = service.users().messages().list(userId='me', labelIds=['INBOX', 'UNREAD']).execute()
             messages = results.get('messages', [])
             if not messages:
